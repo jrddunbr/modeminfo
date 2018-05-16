32b2f508d0448bc624e45d14d20a8c95b2c9d941
Jared Dunbar <jrddunbr@gmail.com>
2018-05-16 01:44:08 -0400

    added loop

--- main.py
+++ main.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
 
 from dpc3216 import dpc3216
-from pprint import pprint
+from time import sleep
 
 modem = dpc3216()
-modem.getPageData()
-modem.getLangData()
-#modem.printPageData()
-if len(modem.pageData) > 10:
-    print("modem data recieved")
-if len(modem.pageData) > 10:
-    print("modem lang recieved")
-modem.parseLang()
-modem.parse()
 
-print()
+while 1:
+    modem.update()
 
-print("Modem Information:")
-for entry in modem.modemInfo:
-    print("\t{}: {}".format(entry, modem.modemInfo[entry]))
+    print("Modem Information:")
+    for entry in modem.modemInfo:
+        print("\t{}: {}".format(entry, modem.modemInfo[entry]))
 
-print("Docsis Information:")
-for entry in modem.docsisInfo:
-    print("\t{}: {}".format(entry, modem.docsisInfo[entry]))
+    print("Docsis Information:")
+    for entry in modem.docsisInfo:
+        print("\t{}: {}".format(entry, modem.docsisInfo[entry]))
 
-print("RX Information:")
-for entry in modem.rx:
-    data = modem.rx[entry]
-    print("\t{}: Power: {}dBmV\tSignal To Noise Ratio: {}dB".format(entry, data[0], data[1]))
+    print("RX Information:")
+    for entry in modem.rx:
+        data = modem.rx[entry]
+        print("\t{}: Power: {}dBmV\tSignal To Noise Ratio: {}dB".format(entry, data[0], data[1]))
 
-print("TX Information:")
-for entry in modem.tx:
-    data = modem.tx[entry]
-    print("\t{}: Power: {}dBmV".format(entry, data))
+    print("TX Information:")
+    for entry in modem.tx:
+        data = modem.tx[entry]
+        print("\t{}: Power: {}dBmV".format(entry, data))
+
+    #sleep(0.1)
