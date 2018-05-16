47833f206793d847aca2e61f6f91ed1f8eb20c17
Jared Dunbar <jrddunbr@gmail.com>
2018-05-15 23:07:59 -0400

    initial commit

---
+++ dpc3216/__init__.py
@@ -0,0 +1,12 @@
+# __init__.py
+# Cisco DPC3216 Status Page Parser
+
+DEFAULT_IP = "192.168.100.1"
+DEFAULT_PAGE = "Docsis_system.asp"
+
+class dpc3216:
+    def __init__(self):
+        print("Hello World, Cisco DPC3216 Library Loaded")
+
+    def getData(self):
+        pass
---
+++ main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+from dpc3216 import dpc3216
+
+modem = dpc3216()
